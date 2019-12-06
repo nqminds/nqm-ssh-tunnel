943ffa09a091a673e5cd96ff78619978e07cd539
Alois Klink <alois.klink@gmail.com>
2019-12-06 21:20:02 +0000

    Make cp's relative to script path

--- install.bash
+++ install.bash
@@ -26,23 +26,28 @@ fi
 # (see: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/)
 set -Eeuo pipefail
 
+# Absolute path to this script. /home/user/bin/foo.sh
+script_path=$(readlink -f "$0")
+# Absolute path this script is in. /home/user/bin
+script_dir=$(dirname "$script_path")
+
 function install-config() {
     if grep -Fxq "$server" "${HOME}/.ssh/config"; then
         # exists already, so return
         return
     else
         # append config to file only if it isn't already in config
-        cat "config" >> "${HOME}/.ssh/config"
+        cat "${script_dir}/config" >> "${HOME}/.ssh/config"
     fi
 }
 
 function install-service() {
     mkdir -p "${HOME}/.local/bin"
-    cp ssh-tunnel.bash "${HOME}/.local/bin/"
+    cp "${script_dir}/ssh-tunnel.bash" "${HOME}/.local/bin/"
     # start user-systemd on boot, not login
     loginctl enable-linger "$USER"
     mkdir -p "${HOME}/.config/systemd/user"
-    cp ssh-tunnel.service "${HOME}/.config/systemd/user/"
+    cp "${script_dir}/ssh-tunnel.service" "${HOME}/.config/systemd/user/"
     systemctl --user daemon-reload
     systemctl --user start ssh-tunnel.service
     systemctl --user enable ssh-tunnel.service
@@ -52,7 +57,7 @@ function install-ssh-key() {
     sudo apt install haveged -y # recommended to increase entropy
     # create SSH key if one does not exist
     if [ ! -f "${HOME}/.ssh/id_ed25519.pub" ]; then
-        ssh-keygen -t id_ed25519 -C "${USR}@$(hostname)" \
+        ssh-keygen -t ed25519 -C "${USER}@$(hostname)" \
         -f "${HOME}/.ssh/id_ed25519" -N ""
     fi
     ssh-copy-id -o "IdentityFile $server_private_key" "$server" -f
